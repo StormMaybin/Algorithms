2e55f4065d5405e6974fde0c1ab9a334d3cccb70
StormMaybin <1325338799@qq.com>
2016-05-25 20:50:32 +0800

    2016-05-25

---
+++ SumQuestion/src/com/stormma/Sum/TwoSum.java
@@ -0,0 +1,37 @@
+﻿/**
+ *@author StormMaybin
+ *@Date 2016-05-25
+ */
+/**
+ * 这个类是暴力算法求一个有序数组和为零的对数
+ * 算法复杂度是平方级
+ */
+package com.stormma.Sum;
+import java.util.Scanner;
+public class TwoSum
+{
+	public static void main(String[] arguments)
+	{
+		int count = 0;
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		int [] a = new int [N];
+		for (int i = 0; i < N; i++)
+		{
+			a[i] = scan.nextInt();
+		}
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = i + 1; j < N; j++)
+			{
+				if (a[i] + a[j] == 0)
+				{
+					count++;
+				}
+			}
+		}
+		System.out.println(count);
+		scan.close();
+	}
+
+}
